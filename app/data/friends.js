51d9b528ea40ae552f48015c992900b778ac2efc
DustinMcGilvray <15370964+DustinMcGilvray@users.noreply.github.com>
2018-09-11 15:16:51 -0500

    Style sheets established, Image folder Created, Background Images linked.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,19 @@
-var friends = [];
+var friends = [
+    {
+        name: Dustin,
+        photo:"../public/images/dustin_headshot.jpg",
+        scores:[
+5,
+4,
+3,
+2,
+1,
+1,
+2,
+3,
+4,
+5
+        ]
+    }
+];
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,11 +2,11 @@ var friends = require("../data/friends");
 
 module.exports = function(app) {
 
-app.GET("/api/friends", function (require, response) {
+app.get("/api/friends", function (require, response) {
     return response.json(friends);
 });
 
-app.POST("/api/friends", function (require, response) {
+app.post("/api/friends", function (require, response) {
     var newFriend = require.body;
 
     //NEED TO .push somewhere
