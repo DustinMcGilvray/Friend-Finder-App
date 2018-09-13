7c04778d339921fd1aaa55356d47a95b4c46efd0
DustinMcGilvray <15370964+DustinMcGilvray@users.noreply.github.com>
2018-09-13 18:25:10 -0500

    Added Modal html, css, rough JS. Added additional Match objects.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,66 @@
-var friends = [
-    {
+var friends = [{
         name: Dustin,
-        photo:"../public/images/dustin_headshot.jpg",
-        scores:[
-5,
-4,
-3,
-2,
-1,
-1,
-2,
-3,
-4,
-5
+        photo: "../public/images/dustin_headshot.jpg",
+        scores: [
+            5,
+            4,
+            3,
+            2,
+            1,
+            1,
+            2,
+            3,
+            4,
+            5
         ]
+    },
+    {
+        name: Marty,
+        photo: "../public/images/eighties_image.jpg",
+        scores: [
+            4,
+            4,
+            3,
+            3,
+            1,
+            2,
+            2,
+            3,
+            4,
+            4
+        ]
+
+    },
+    {
+        name: Doc,
+        photo: "../public/images/rubicks_cube.jpg",
+        scores: [
+            4,
+            4,
+            3,
+            3,
+            1,
+            2,
+            2,
+            3,
+            4,
+            4
+        ]
+    },
+    {
+        name: Biff,
+        photo:"../public/images/i_love_eighties.jpg",
+        scores:[
+1,
+4,
+3,
+5,
+1,
+2,
+4,
+3,
+1,
+4
     }
 ];
 module.exports = friends;
